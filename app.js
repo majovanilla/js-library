74bda1838192e1fd09e47feeb3f8a81972558cbf
sumancrest0001 <suman.crest0001@gmail.com>
2020-01-17 21:05:44 +0545

    added the render function

--- app.js
+++ app.js
@@ -21,17 +21,18 @@ function Book(title, author, pages, read) {
 
 function createBook(book) {
   myLibrary.push(book);
+  document.querySelector('.input-form').classList.toggle('form-visibility');
+  render(myLibrary);
 }
 
 function addBookToLibrary() {
-  const bookTitle = document.getElementById('book-title').value;
-  console.log(bookTitle);
-  const authorName = document.getElementById('author-name').value;
-  const description = document.getElementById('book-description').value;
-  const pagesNum = document.getElementById('book-pages').value;
-  const status = document.getElementById('read-status').value;
-
-  createBook(new Book(bookTitle, authorName, description, pagesNum, status));
+  var bookTitle =  document.getElementById("book-title").value;
+  const authorName = document.getElementById("author-name").value;
+  const description = document.getElementById("book-description").value;
+  const pagesNum = document.getElementById("book-pages").value;
+  const status = document.getElementById("read-status").value;
+  let book1 =new Book(bookTitle, authorName, description, pagesNum, status);
+  createBook(book1);
 }
 
 
@@ -45,10 +46,29 @@ function toggleForm(button) {
 }
 
 // function for handling click event on Submit.
-submitForm(submitBook);
+submitBook.addEventListener("click", addBookToLibrary);
+function render(arr){
+   for (let book of arr) {
+    let readingStatus, parentElement, html;
+    readingStatus = book.status === "read" ? "You have read this book" : "you haven't read this book";
+    html = `<div class= "book-card"><h3 class="book-title">${book.title}</h3>
+            <p class="author-name">by ${book.author}</p>
+            <p class="description">${book.description}</p>
+            <div class="book-details">
+               <p class="book-pages"><i class="fa fa-chevron-right"></i> No. of Pages: <span>${book.pages}</span></p>
+                <p><i class="fa fa-chevron-right"></i> ${readingStatus}</p>
+            </div>
+            <div class="btns">
+              <button class="btn-read-status book-buttons">Read</button>
+              <button class="btn-remove book-buttons">Delete</button>
+              <button class="btn-edit book-buttons">Edit</button>
+            </div>
+        </div>`;
+    parentElement = document.querySelector('#book-list');
+    parentElement.insertAdjacentHTML('beforeend', html);
 
-function submitForm(button) {
-  button.addEventListener('click', addBookToLibrary());
+
+   }
+
 }
 
-console.log(myLibrary);
