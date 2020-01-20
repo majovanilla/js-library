94d4e8e13a1bee70196c76cf90e6201714333037
sumancrest0001 <suman.crest0001@gmail.com>
2020-01-20 20:14:48 +0545

    read unread functionality implemented

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const removeBook = document.querySelector('.btn-remove');
 const editBook = document.querySelector('.btn-edit');
 const form = document.querySelector('.input-form');
 
+myLibrary.push(new Book('123ser', 'test book', 'test author', 'test book description', 1234, 'read'));
+myLibrary.push(new Book('123r', 'test book 2', 'test author random', 'test book description 2', 134, ''));
+if (myLibrary.length > 0)
+{
+  for(let book of myLibrary){
+    render(book.id, book.title, book.author, book.description, book.pages, book.read);
+  }
+}
 function Book(id, title, author, description, pages, read) {
     this.id = id;
     this.title = title;
@@ -32,13 +40,13 @@ function addBookToLibrary() {
   const authorName = document.getElementById("author-name").value;
   const description = document.getElementById("book-description").value;
   const pagesNum = document.getElementById("book-pages").value;
-  const status = document.getElementById("read-status").value;
+  const status = document.querySelector('input[name="read"]:checked').value;
   const id = randomId();
   let book =new Book(id, bookTitle, authorName, description, pagesNum, status);
   let valid = inputValid(bookTitle, authorName, description, pagesNum);
 
-  if (valid == true) {createBook(book)};
-  render(id, bookTitle, authorName, description, pagesNum, status);
+  if (valid == true) {createBook(book);
+  render(id, bookTitle, authorName, description, pagesNum, status);}
   form.reset();
 }
 
@@ -56,28 +64,29 @@ submitBook.addEventListener("click", addBookToLibrary);
 
 function render(id, title, author, description, pages, status){
     let readingStatus, parentElement, html;
-    readingStatus = status === "read" ? "You have read this book" : "you haven't read this book";
+    console.log(status);
+    readingStatus = status === "read" ? "You have read this book" : "You haven't read this book";
     bookStatus = status === "read" ? "Unread" : "Read";
     html = `<div class= "book-card" id="${id}"><h3 class="book-title">${title}</h3>
-            <p class="author-name">by ${author}</p>
-            <p class="description">${description}</p>
-            <div class="book-details">
-               <p class="book-pages"><i class="fa fa-chevron-right"></i> No. of Pages: <span>${pages}</span></p>
-                <p><i class="fa fa-chevron-right"></i> ${readingStatus}</p>
-            </div>
-            <div class="btns">
-              <button class="btn-read-status book-buttons">${bookStatus}</button>
-              <button class="btn-remove book-buttons">Delete</button>
-              <button class="btn-edit book-buttons">Edit</button>
-            </div>
-        </div>`;
-    
-    parentElement = document.querySelector('#book-list').insertAdjacentHTML('beforeend', html); 
+              <p class="author-name">by ${author}</p>
+              <p class="description">${description}</p>
+              <div class="book-details">
+                 <p class="book-pages"><i class="fa fa-chevron-right"></i> No. of Pages: <span>${pages}</span></p>
+                  <p><i class="fa fa-chevron-right"></i> ${readingStatus}</p>
+              </div>
+              <div class="btns">
+                <button class="btn-read-status book-buttons">${bookStatus}</button>
+                <button class="btn-remove book-buttons">Delete</button>
+                <button class="btn-edit book-buttons">Edit</button>
+              </div>
+            </div>`;
+
+    parentElement = document.querySelector('#book-list').insertAdjacentHTML('beforeend', html);
 }
 
-function modifyStatus(book) {
+function modifyStatus(id) {
   changeStatus.addEventListener('click', function() {
-    document.getElementById(`"${book.id}"`)
+    document.getElementById(id);
   })
 }
 
@@ -93,4 +102,12 @@ function randomId() {
   return '_' + Math.random().toString(36).substr(2, 9);
 };
 
+
+function findBookID(btn){
+  let parentNodeID, ids, indexOfBook;
+  parentNodeID = btn.parentNode.parentNode.id;
+  ids = myLibrary.map(cur => return cur.id);
+  indexOfBook = ids.indexOf(parentNodeID);
+  return indexOfBook;
+}
 console.log(myLibrary);
